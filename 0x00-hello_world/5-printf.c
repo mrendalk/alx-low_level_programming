42119c3aa9420ecc23fb918429bd65b79ad4d71f
mrendalk <endalk234@gmail.com>
2022-09-12 11:58:25 +0900

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,12 @@
  * main - Entry point
  *
  * Description - printing file
+ * Return: 0
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
 
