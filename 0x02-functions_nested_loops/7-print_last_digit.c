27ad6bc9354e51ac4eeee0cf954086f43fabf892
mrendalk <endalk234@gmail.com>
2022-09-14 17:56:48 +0900

    function that computes the absolute value of an intger

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,6 +8,13 @@
 
 int print_last_digit(int c)
 {
-	return (c % 10);
-	return (c % 10);
+	int last_digit;
+
+	last_digit = (c % 10);
+	if (last_digit < 0)
+	{
+		last_digit *= -1;
+	}
+	_putchar('0');
+	return (last_digit);
 }
