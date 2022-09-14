49a3a8e933420d3372acf7bbd638141626a83ac2
mrendalk <endalk234@gmail.com>
2022-09-14 17:25:26 +0900

    function that computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - absoulute value of an integer
+ * @c: argument
+ * Return: result
+ */
+int _abs(int c)
+{
+	if(c > 0 || c <0)
+	{
+		return (c);
+	}
+	else
+	{
+		return (c * -1);
+	}
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of a number
+ *
+ * Return: the result
+ */
+
+int print_last_digit(int c)
+{
+	int result, r;
+	result = c % 10;
+	return ("%d", result);
+}
