6258c4e8a642fcb13760070ceb9c4466208c26d3
mrendalk <endalk234@gmail.com>
2022-09-29 08:50:54 +0900

    advanced

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
+* is_palindrome - check whether palindreome of not
 * @s: string to be a printed
 * Return: void
 */
 int is_palindrome(char *s)
 {
 	int flag = 1;
+
 	check(s, 0, _strlen_recursion(s) - 1, &flag);
 	return (flag);
 
 }
-#include "main.h"
 /**
 * check - prints's a string followed
 * @s: string to be a printed
@@ -19,7 +20,6 @@ int is_palindrome(char *s)
 * @flag: flag
 * Return: void
 */
-
 void check(char *s, int start, int end, int *flag)
 {
 	if (start <= end)
@@ -28,7 +28,7 @@ void check(char *s, int start, int end, int *flag)
 			*flag *= 1;
 	else
 		*flag *= 0;
-	check(s, start + 1, end -1, flag);
+	check(s, start + 1, end - 1, flag);
 	}
 
 }
@@ -40,6 +40,7 @@ void check(char *s, int start, int end, int *flag)
 int _strlen_recursion(char *s)
 {
 	int sum = 0;
+
 	if (*s != '\0')
 	{
 		sum++;
