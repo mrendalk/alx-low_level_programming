c309b5e41c5c9feed4346e826a6bd393855c71e1
mrendalk <endalk234@gmail.com>
2022-10-01 00:26:49 +0900

    task 0 print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print command line argument
+ * @argc: number of command line argument
+ * @argv: array of command line argument
+ * Retrun: exit with 0
+ */
+
+int main(int argc, char* argv)
+{
+	printf("%s",argv[argc]);
+	return 0;
+}
